8327d94b6664fd9af7468809beb1eb487771e559
feernando <fernandofitilan@hotmail.com>
2021-01-04 10:10:19 -0300

    up

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    name: 'Maria',
+    title: 'Gerente',
+    quote:
+      ' Teste',
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    name: 'Carlos',
+    title: 'Dev',
+    quote:
+      'Teste',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
+    name: 'João',
+    title: 'estagiário',
+    quote:
+      'Teste',
+  },
+  {
+    id: 4,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    name: 'Susana',
+    title: 'chefe',
+    quote:
+      'Teste',
+  },
+];
+
+export default people;
